4c4bafa6389a3272bf7a4f6ee70ae2378d4ac446
Ian <Ian.Mohn1@gmail.com>
2022-03-20 10:18:10 -0500

    added psuedocode to help track the completion of the criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,28 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Assignment code here
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  console.log("Try to guess this password!");
+
+      //PSUEDOCODE: 
+  // 1. need to prompt the user for password criteria
+  //    a. Password length needs to be between 8-128 characters
+  //    b. Lowercase, uppercase, numbers, special characters
+  // 2. Need to validate the input with at least one character type that will be selected
+  // 3. Generate password based on the provided criteria.
+
+  // 4. Display generated password to the page - COMPLETED
+
+
+  return "Generated password will go here!";
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
